72d5187517805ad521468fbc9d7c78533ab981c3
Taras <tapacpro@gmail.com>
2021-06-29 18:10:36 +0300

    Update to next.js@11
    - rewrite to use next lint from the box
    - conditional image component rendering
    - remove /components folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,15 +28,17 @@ const IndexPage = () => {
     return (
       <div className="flex justify-center mt-8 text-center">
         <div className="flex-auto">
-          <Image
-            // @ts-ignore
-            src={session.user.image}
-            // @ts-ignore
-            alt={session.user.email ?? session.user.name}
-            width="60"
-            height="60"
-            className="h-16 w-16 rounded-full mx-auto mb-2"
-          />
+          {session.user.image && (
+            <Image
+              // @ts-ignore
+              src={session.user.image}
+              // @ts-ignore
+              alt={session.user.email ?? session.user.name}
+              width="60"
+              height="60"
+              className="h-16 w-16 rounded-full mx-auto mb-2"
+            />
+          )}
           <div className="text-lg mb-2">Hello, {session.user.email ?? session.user.name}</div>
           <div className="mb-2">
             gql test query: {queryLoading ? 'fetching...' : data?.example?.message}
